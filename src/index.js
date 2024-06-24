16b526c29d3db1eeaf67ce573281fd4f4064ce6e
essien emmanuel <essienemma300@gmail.com>
2024-06-24 08:39:50 +0100

    generating a link url

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ document.querySelector('form').addEventListener('submit', event => {
   const msgInput = document.querySelector('#message-input');
   const encryptedMsg = btoa(msgInput.value);
   
-  document.querySelector('#link-input').value = encryptedMsg;
+  const browseerLocalFilePath = window.location;
+  const linkInput = document.querySelector('#link-input')
+  linkInput.value =`${browseerLocalFilePath}#${encryptedMsg}`;
+  linkInput.select()
 })
