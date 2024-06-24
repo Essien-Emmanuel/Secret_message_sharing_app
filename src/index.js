bd2f7d03d8e7cc5f1372148916cb8e71f2f720e5
essien emmanuel <essienemma300@gmail.com>
2024-06-24 08:23:45 +0100

    getting input from form, encypting and displaying for copy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('send a message')
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+
+  const msgInput = document.querySelector('#message-input');
+  const encryptedMsg = btoa(msgInput.value);
+  
+  document.querySelector('#link-input').value = encryptedMsg;
+})
