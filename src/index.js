b924073651f5e9a7b934842004b740c7f63b0625
essien emmanuel <essienemma300@gmail.com>
2024-06-24 08:59:44 +0100

    toggle message and link card panel visibility

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ document.querySelector('form').addEventListener('submit', event => {
   const msgInput = document.querySelector('#message-input');
   const encryptedMsg = btoa(msgInput.value);
   
+  // toggle message and link card panels visibility
+  document.querySelector('#message-form').classList.add('hide')
+  document.querySelector('#message-link').classList.remove('hide')
+  
   const browseerLocalFilePath = window.location;
   const linkInput = document.querySelector('#link-input')
   linkInput.value =`${browseerLocalFilePath}#${encryptedMsg}`;
-  linkInput.select()
+  
+  linkInput.select();
+
+
 })
